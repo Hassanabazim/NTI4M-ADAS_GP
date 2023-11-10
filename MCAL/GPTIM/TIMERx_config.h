2975397d95153da38a618d651f76063aff9e4304
ziad.hamdy <engziadhamdy@gmail.com>
2023-11-10 15:36:27 +0200

    Timer & DC MOTOR Driver

---
+++ MCAL/GPTIM/TIMERx_config.h
@@ -0,0 +1,44 @@
+/*
+ * TIMERx_config.h
+ *
+ *  Created on: Nov 3, 2023
+ *      Author: Mario Ezzat
+ */
+
+#ifndef MCAL_TIMERX_TIMERX_CONFIG_H_
+#define MCAL_TIMERX_TIMERX_CONFIG_H_
+
+/* Options:- TIMER_ENABLE  -  TIMER_DISABLE */
+#define TIMER_2_ENABLE 						TIMER_DISABLE
+/* Options:- TIMER_COUNT_UP  -  TIMER_COUNT_DOWN */
+#define TIMER_2_DIRECTION 					TIMER_COUNT_UP
+/* Options:- 0.... 65565 */
+#define TIMWER_2_PRESCALER					 1
+#define TIMER_2_MAX_VALUE 					 65535
+
+
+/* Options:- TIMER_ENABLE  -  TIMER_DISABLE */
+#define TIMER_3_ENABLE 					TIMER_ENABLE
+/* Options:- TIMER_COUNT_UP  -  TIMER_COUNT_DOWN */
+#define TIMER_3_DIRECTION 				TIMER_COUNT_UP
+/* Options:- 0.... 65565 */
+#define TIMWER_3_PRESCALER 				1
+#define TIMER_3_MAX_VALUE 				65535
+
+/* Options:- TIMER_ENABLE  -  TIMER_DISABLE */
+#define TIMER_4_ENABLE TIMER_DISABLE
+/* Options:- TIMER_COUNT_UP  -  TIMER_COUNT_DOWN */
+#define TIMER_4_DIRECTION TIMER_COUNT_UP
+/* Options:- 0.... 65565 */
+#define TIMWER_4_PRESCALER 1
+#define TIMER_4_MAX_VALUE 65535
+
+/* Options:- TIMER_ENABLE  -  TIMER_DISABLE */
+#define TIMER_5_ENABLE				 TIMER_DISABLE
+/* Options:- TIMER_COUNT_UP  -  TIMER_COUNT_DOWN */
+#define TIMER_5_DIRECTION 			TIMER_COUNT_UP
+/* Options:- 0.... 65565 */
+#define TIMWER_5_PRESCALER 				1
+#define TIMER_5_MAX_VALUE 				65535
+
+#endif /* MCAL_TIMERX_TIMERX_CONFIG_H_ */
